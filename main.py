865dae6dc4d2aba9a8e0fe1f71e039a7b69ebade
Michael Slice <mjsens523@gmail.com>
2023-06-06 00:01:13 -0500

    Updates

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ rs.login(username=robin_user,
          expiresIn=86400,
          by_sms=True)
 
+
+def get_tickers():
+    tickers = rs.get_top_100()
+    stocks = []
+
+def get_user_stocks(ticker_list):
+    with open("ticker_list", 'r') as read:
+        user_stocks = read.readlines()
+        for i in (len(user_stocks)):
+            user_stocks[i] = user_stocks[i].strip("\i")
+        return user_stocks 
+
+
+
+
+
+
+
+
+
+
