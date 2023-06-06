d661585dedd61e24a9cb8766d139e3c8da8cba47
Michael Slice <mjsens523@gmail.com>
2023-06-05 20:05:25 -0500

    Added Login

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import robin_stocks as rs
+import robin_stocks.robinhood as rs
+import os 
+
+robin_user = os.environ.get("robinhood_username")
+robin_pass = os.environ.get("robinhood_password")
+
+rs.login(username=robin_user,
+         password=robin_pass,
+         expiresIn=86400,
+         by_sms=True)
 
