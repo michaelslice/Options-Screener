2264f4b0bdefffafaf8160345a251fb094e0aa0d
Michael Slice <mjsens523@gmail.com>
2023-06-06 01:21:41 -0500

    Updates

--- main.py
+++ main.py
@@ -19,19 +19,23 @@ def get_tickers():
                 tickers.append(tickers[number][element])
     return stocks
 
-def get_user_stocks(ticker_list):
-    with open("ticker_list", 'r') as read:
+def get_tickers_stocks(file_name):
+    with open(file_name, 'r') as read:
         user_stocks = read.readlines()
         for i in (len(user_stocks)):
             user_stocks[i] = user_stocks[i].strip("\i")
         return user_stocks 
 
-
-
-
-
-
-
+def filter_ticker_by_price(stocks):
+    stocks_under_price = {}
+    for stock in stocks:
+        new_stocks = rs.get_quotes(stocks)
+        for new_stock in new_stocks:
+            if float(new_stock["last_trade_price"]) <= price_to_filter:
+                local_stock = new_stock["symbol"]
+                stocks_under_price[local_stock] = new_stock["last_trade_price"]
+    print(stocks_under_price)
+    return stocks_under_price 
 
 
 
